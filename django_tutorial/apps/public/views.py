79ae003a7470d782db6c7a6fd5aceca16b3f14fd
Chris <chrisbatey971@gmail.com>
2022-02-13 17:35:32 -0500

    x g G
    x
    X
    X

---
+++ django_tutorial/apps/public/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+ 
